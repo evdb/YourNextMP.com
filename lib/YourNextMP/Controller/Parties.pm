4a193be2012415b21120e97184baa6c37b20c8e0
Edmund von der Burg <evdb@ecclestoad.co.uk>
2010-02-22 10:50:59 -0300

    List candidates for a party on a seperate page

--- lib/YourNextMP/Controller/Parties.pm
+++ lib/YourNextMP/Controller/Parties.pm
@@ -12,4 +12,39 @@ sub source_name {
     return 'Party';
 }
 
+sub candidates_empty : PathPart('candidates') Chained('result_find') {
+    my ( $self, $c ) = @_;
+
+    $c->res->redirect(
+        $c->uri_for( $c->stash->{result}->code, 'candidates', 1 ) );
+    $c->detach;
+}
+
+sub candidates : PathPart('candidates') Chained('result_find') Args(1) {
+    my ( $self, $c, $page_number ) = @_;
+
+    my $results_per_page = 50;
+
+    # clean up the page_number
+    $page_number =~ s{\D+}{}g;
+    $page_number ||= 1;
+
+    my $results = $c->stash->{result}->candidates->search(
+        undef,    # find everything
+        {
+            rows => $results_per_page,
+            page => $page_number,
+        }
+    );
+
+    # check that we have not gone beyond the end of the list
+    if ( $page_number > $results->pager->last_page ) {
+        $c->res->redirect( $c->uri_for( 'all', $results->pager->last_page ) );
+        $c->detach;
+    }
+
+    $c->stash->{pager}   = $results->pager;
+    $c->stash->{results} = $results;
+}
+
 1;
