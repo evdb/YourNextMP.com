71ad3bce1e99ba8e9d2b2cac8cd53d60e2dcd713
Edmund von der Burg <evdb@ecclestoad.co.uk>
2010-03-17 17:45:32 +0000

    Split out the bad_details css and js into separate files

--- root/static/js/bad_details.js
+++ root/static/js/bad_details.js
@@ -1,3 +1,29 @@
 function open_in_iframe ( url ) {
     $('iframe').attr({ "src" : url });
 }
+
+$( function () {
+    
+    // create the extra field links
+    $('div.secondary').each(
+        function ( index, element ) {
+            var a     = $('<a href="#"/>');
+            var li    = $('<li></li>');
+            var name = $('label', element ).attr('for');
+
+            if ( !name )
+                return true ;
+
+            a.html( name );
+            a.click(function() {
+                $(element).slideToggle();
+                return false;
+            });
+
+            li.append( a );
+
+            $('#extra_fields').append( li ).show();
+        }
+    );
+        
+} );
