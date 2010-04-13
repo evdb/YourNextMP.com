e32a7ac0b67b0123ccd927facf567af84a47f5f8
Edmund von der Burg <evdb@ecclestoad.co.uk>
2010-04-13 12:55:22 +0100

    List seats with duplicate candidates

--- lib/YourNextMP/Controller/Admin.pm
+++ lib/YourNextMP/Controller/Admin.pm
@@ -18,4 +18,31 @@ sub index : Path('') {
     # passthrough
 }
 
+sub duplicate_candidates : Local {
+    my ( $self, $c ) = @_;
+
+    my @seats_with_duplicates = ();
+    my $all_seats             = $c->db('Seat');
+    my @party_ids_to_ignore =
+      map { $c->db('Party')->find( { code => $_ } )->id } qw(independent);
+
+    while ( my $seat = $all_seats->next ) {
+
+        # count the occurences of the parties
+        my %party_counts = ();
+        $party_counts{$_}++ for map { $_->party_id } $seat->candidates;
+
+        # delete the independents etc.
+        delete $party_counts{$_} for @party_ids_to_ignore;
+
+        # skip on unless there are duplicates
+        next unless grep { $_ > 1 } values %party_counts;
+
+        push @seats_with_duplicates, $seat;
+    }
+
+    $c->stash->{seats_with_duplicates} = \@seats_with_duplicates; 
+
+}
+
 1;
