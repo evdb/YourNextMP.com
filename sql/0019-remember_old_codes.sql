dad5f8bd58af52df67648b64c3be9345d89adee9
Edmund von der Burg <evdb@ecclestoad.co.uk>
2010-04-19 13:46:00 +0100

    create the code rename table

---
+++ sql/0019-remember_old_codes.sql
@@ -0,0 +1,9 @@
+begin;
+
+create table code_renames (
+    id          bigint      default nextval('global_id_seq') primary key,
+    old_code    varchar(80) not null,
+    new_code    varchar(80) not null
+);
+
+commit;
