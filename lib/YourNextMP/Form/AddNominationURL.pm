eeb016818b858ff22c2ef10766931ca8e402e810
Edmund von der Burg <evdb@ecclestoad.co.uk>
2010-04-21 08:57:55 +0100

    Wording tweaks and ditching the number of candidates

--- lib/YourNextMP/Form/AddNominationURL.pm
+++ lib/YourNextMP/Form/AddNominationURL.pm
@@ -13,21 +13,21 @@ has '+field_traits' => (    #
 
 has_field 'nomination_url' => (
     type     => '+YourNextMP::Form::Field::WebAddress',
-    label    => 'URL to nominations',
+    label    => 'Link to official list',
     required => 1,
     required_message =>
       'Please enter the web address for the page with the nominations',
     hint => 'Please find a web page that has the nominations',
 );
 
-has_field 'nominated_count' => (
-    type             => 'Integer',
-    label            => 'number of nominated candidates',
-    required         => 1,
-    required_message => 'Please enter the number of candidates.',
-    hint => 'Please count all the candidates and put the total here.'
-      . ' This lets us check that we have an entry for all the candidates.',
-);
+# has_field 'nominated_count' => (
+#     type             => 'Integer',
+#     label            => 'number of nominated candidates',
+#     required         => 1,
+#     required_message => 'Please enter the number of candidates.',
+#     hint => 'Please count all the candidates and put the total here.'
+#       . ' This lets us check that we have an entry for all the candidates.',
+# );
 
 has_field 'submit' => (
     type  => 'Submit',
