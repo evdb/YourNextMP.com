3a37e7ed73c13cd81c289d107cea3e6ebfe23916
Edmund von der Burg <evdb@ecclestoad.co.uk>
2010-07-03 10:45:28 +0100

    cron scripts no longer needed

--- script/cron/daily_cron.bash
+++ script/cron/daily_cron.bash
@@ -4,6 +4,13 @@ cd /var/www/yournextmp_production
 
 eval $(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)
 
-./script/cron/party_scraper.pl
+# ./script/cron/backup_database.pl
 
-./script/cron/add_hustings_dot_com_links.pl
+# ./script/cron/generate_data_files.pl
+
+# ./script/cron/party_scraper.pl
+
+# ./script/cron/add_hustings_dot_com_links.pl
+
+# ./script/cron/create_sitemap.pl
+
